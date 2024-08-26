60d7cb8203823c39669c4080898631e5fed6b69a
Jeffrey Frederick <jeffreyfrederick2005@gmail.com>
2024-08-25 23:30:52 -0700

    Initialized a MAX30105 object and calculated the BPM and IR level of the sensor

--- esp/esp.ino
+++ esp/esp.ino
@@ -1,13 +1,23 @@
+#include <Wire.h>
 #include <WiFi.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 #include "MAX30105.h"
+#include "heartRate.h"
 
 #define BME280_ADDRESS 0x76
 
 Adafruit_BME280 bme;
 
 MAX30105 particleSensor;
+
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+
+float beatsPerMinute;
+int beatAvg;
 
 const char* ssid = "Dodgers#1";
 const char* password = "calmhorse123";
@@ -32,9 +42,11 @@ void setup() {
     Serial.println("");
   }
 
-  if (particleSensor.begin()) {
+  if (particleSensor.begin(Wire, I2C_SPEED_FAST)) {
     Serial.println("Connected to the MAX30102 Sensor!");
   }
+
+  particleSensor.setup(); //Configure sensor with default settings
 }
 
 float toFahrenheit(float temp) {
@@ -43,6 +55,29 @@ float toFahrenheit(float temp) {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  long irValue = particleSensor.getIR();
+
+  if (checkForBeat(irValue) == true)
+  {
+    //We sensed a beat!
+    long delta = millis() - lastBeat;
+    lastBeat = millis();
+
+    beatsPerMinute = 60 / (delta / 1000.0);
+
+    if (beatsPerMinute < 255 && beatsPerMinute > 20)
+    {
+      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+      rateSpot %= RATE_SIZE; //Wrap variable
+
+      //Take average of readings
+      beatAvg = 0;
+      for (byte x = 0 ; x < RATE_SIZE ; x++)
+        beatAvg += rates[x];
+      beatAvg /= RATE_SIZE;
+    }
+  }
+  
   Serial.print("Temperature: ");
   Serial.print(toFahrenheit(bme.readTemperature()));
   Serial.println("°F");
@@ -56,5 +91,15 @@ void loop() {
   Serial.println("%");
   Serial.println("");
 
-  delay(3000);
+  Serial.print("IR= ");
+  Serial.print(irValue);
+  Serial.print(", BPM= ");
+  Serial.print(beatsPerMinute);
+  Serial.print(", Avg BPM= ");
+  Serial.print(beatAvg);
+
+  if (irValue < 50000) {
+    Serial.print(" No finger?");
+  }
+  Serial.println("");
 }
